4a93798fcd221467bd0586ad9842783c2214977b
ShihYuan <shmily7829@gmail.com>
2022-11-24 16:20:42 +0800

    加入專案檔案。

---
+++ Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorServerAuthenticationAndAuthorzation.Authentication
+{
+    /// <summary>
+    /// https://youtu.be/iq2btD9WufI?t=370
+    /// </summary>
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
